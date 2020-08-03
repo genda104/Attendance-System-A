6e385f81f34877896629b5b1f047e08b6e3657ed
genda104 <prnd@team.email.ne.jp>
2020-08-03 10:51:41 +0000

    base revise

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,21 +1,26 @@
 class BasesController < ApplicationController
-  before_action :set_base, only: [:show, :update, :destroy]
+  before_action :set_base, only: [:show, :edit, :update, :destroy]
   before_action :admin_user
   
   def index
-    @bases = Base.all.order('base_id ASC')
     @base = Base.new
+    @bases = Base.all.order('base_id ASC')
+  end
+
+  def show
   end
   
   def new
+    @base = Base.new
   end
   
   def create
     @base = Base.new(base_params)
     if @base.save
-      flash[:success] = '拠点情報を追加しました。'  
+      flash[:success] = '拠点情報を作成しました。'  
       redirect_to bases_url
     else
+      flash[:danger] = '拠点情報の作成に失敗しました。'
       render :index
     end
   end
@@ -28,10 +33,16 @@ class BasesController < ApplicationController
       flash[:success] = "拠点情報を更新しました。"
       redirect_to bases_url
     else
+      flash[:danger] = '拠点情報の更新に失敗しました。'
       render :index
     end
   end
   
+  def destroy
+    @base.destroy
+    flash[:success] = "#{@base.name}のデータを削除しました。"
+    redirect_to bases_url
+  end
   
   private
   
--- app/models/base.rb
+++ app/models/base.rb
@@ -1,4 +1,5 @@
 class Base < ApplicationRecord
-  validates :base_id, presence: true, uniqueness: true, numericality: { only_integer: true }
+  validates :base_id, uniqueness: true, numericality: { :greater_than_or_equal_to => 0 }
   validates :name, presence: true
+  validates :attendance, inclusion: { in:["出勤", "退勤"]}
 end
